5e7482db435ab5472b1f66348cb1b9a412cf8e0f
JimenaOcampo <jimena.a.ocampo@hotmail.com>
2023-04-11 21:18:24 -0300

    segundo commit

---
+++ Test/alumno/edu/ar/jime/PruebaProj.java
@@ -0,0 +1,10 @@
+package alumno.edu.ar.jime;
+
+public class PruebaProj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/alumno/edu/ar/jime/MiPrimerProj.java
@@ -0,0 +1,5 @@
+package alumno.edu.ar.jime;
+
+public class MiPrimerProj {
+
+}
